e01877c1bfa734090d3355a43117034a7809ae69
wflan3 <wflanagan3@gatech.edu>
2022-02-26 15:04:03 -0800

    bug fix

--- code/@CRCM/calculateStrainEnergy.m
+++ code/@CRCM/calculateStrainEnergy.m
@@ -2,5 +2,5 @@ function strain_energy = calculateStrainEnergy(obj, theta)
 % calculateStrainEnergy
 %   Calculates the strain energy within the chain for joint angles theta
 
-    strain_energy = sum(0.5*(obj.k*(theta.^2)));
+    strain_energy = sum(0.5*(obj.k.*(theta.^2)));
 end
